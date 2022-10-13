8e4a049c41f412cb2debc70e151d2109a959e357
Luke Eltiste <luke343279@gmail.com>
2022-10-13 10:51:09 -0700

    move files

---
+++ hw4.py
@@ -0,0 +1,8 @@
+from search import EightPuzzle, astar_search, breadth_first_graph_search, depth_first_graph_search
+
+
+if __name__ == "__main__":
+    eight_puzzle = EightPuzzle((1, 2, 3, 5, 7, 4, 8, 6, 0))
+    print(astar_search(eight_puzzle, h=None, display=True).solution())
+    print(breadth_first_graph_search(eight_puzzle).solution())
+    print(depth_first_graph_search(eight_puzzle).solution())
